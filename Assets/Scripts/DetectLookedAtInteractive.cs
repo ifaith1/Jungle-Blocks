8d01bb2aa6674be062e438df99cd641e6538b966
Val <applejelly@yahoo.com>
2019-03-09 17:27:06 -0600

    Scripts and extra little things

---
+++ Assets/Scripts/DetectLookedAtInteractive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects interactive elemennts the player is looking at
+/// 
+/// </summary>
+
+public class DetectLookedAtInteractive : MonoBehaviour
+{
+    [Tooltip("Starting point of raycast used to detect interactives.")]
+    [SerializeField]
+    private Transform raycastOrigin;
+
+    [Tooltip("How far from the raycastOrigin we will search for interactive elements.")]
+    [SerializeField]
+    private float maxRange = 5.0f;
+
+    //private Vector3 raycastDirection;
+
+    private void FixedUpdate()
+    {
+        //Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, maxRange);
+        Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward, Color.red);
+        RaycastHit hitInfo;
+        bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange);
+
+        if (objectWasDetected)
+        {
+            Debug.Log($"Player is looking at: {hitInfo.collider.gameObject.name}");
+        }
+    }
+}
