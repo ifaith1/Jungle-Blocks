ec6e3375c90c1c430523d744b840e94b30174089
Val <applejelly@yahoo.com>
2019-04-11 00:32:23 -0500

    Inventory Objects, Using The Debugger

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
+    [Tooltip("Text that will display in the UI when the player looks at this object in the world.")]
     [SerializeField]
     protected string displayText = nameof(InteractiveObject);
 
---
+++ Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : InteractiveObject
+{
+    [Tooltip("The name of the object, as it will appear in the inventory menu UI.")]
+    [SerializeField]
+    private string objectName;
+
+    // TODO: Add long description field
+    //TODO: Add icon field
+
+    private new Renderer renderer;
+    private new Collider collider;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        collider = GetComponent<Collider>();
+    }
+
+    public InventoryObject ()
+    {
+        displayText = $"Take {objectName}";
+    }
+
+
+        /// <summary>
+        /// When the player interacts with an inventory object, we need to do 2 things:
+        /// 1. Add the inventory object to the PlayerInventory list
+        /// 2. Remove the object from the game world / scene
+        /// Can't use Destroy, because I need to keep the gameobject in the inventory list.
+        /// So we just disable the collider and renderer
+        /// </summary>
+
+    public override void InteractWith()
+    {
+        base.InteractWith();
+        PlayerInventory.InventoryObjects.Add(this);
+        renderer.enabled = false;
+        collider.enabled = false;
+    }
+}
