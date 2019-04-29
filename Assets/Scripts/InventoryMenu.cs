a38b84fd44c71295d9bc55a215e2738524c47819
Val <applejelly@yahoo.com>
2019-04-29 15:10:00 -0500

    Inventory Menu Show/ Hide Cursor

--- Assets/Scripts/InventoryMenu.cs
+++ Assets/Scripts/InventoryMenu.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class InventoryMenu : MonoBehaviour
 {
     private static InventoryMenu instance;
     private CanvasGroup canvasGroup;
-
+    private RigidbodyFirstPersonController rigidbodyFirstPersonController;
     public static InventoryMenu Instance
     {
         get
@@ -26,6 +27,9 @@ public class InventoryMenu : MonoBehaviour
     {
         canvasGroup.alpha = 1;
         canvasGroup.interactable = true;
+        rigidbodyFirstPersonController.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
 
@@ -33,6 +37,8 @@ public class InventoryMenu : MonoBehaviour
     {
         canvasGroup.alpha = 0;
         canvasGroup.interactable = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        rigidbodyFirstPersonController.enabled = true;
     }
 
     private void Update()
@@ -58,6 +64,11 @@ public class InventoryMenu : MonoBehaviour
             throw new System.Exception("There is alreadya n instance of InventoryMenu and there can only be one.");
 
         canvasGroup = GetComponent<CanvasGroup>();
+        rigidbodyFirstPersonController = FindObjectOfType<RigidbodyFirstPersonController>();
+    }
+
+    private void Start()
+    {
         HideMenu();
     }
 }
