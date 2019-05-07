d8c53e408c03fa6dbdfe97b8a2dbaf6cc0c765a8
Val <applejelly@yahoo.com>
2019-05-07 14:58:19 -0500

    Credits Finished

---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenUnicornFont()
+    {
+        Application.OpenURL("https://www.dafont.com/unicornflakes.font");
+    }
+
+    public void OpenPaper()
+    {
+        Application.OpenURL("https://freesound.org/people/InspectorJ/sounds/415766/");
+    }
+
+    public void OpenDrip()
+    {
+        Application.OpenURL("https://freesound.org/people/kwahmah_02/sounds/260618/");
+    }
+
+    public void OpenBleeeh()
+    {
+        Application.OpenURL("https://freesound.org/people/scuzzpuck/sounds/40838/");
+    }
+
+    public void OpenPurplePlanet()
+    {
+        Application.OpenURL("https://www.purple-planet.com/");
+    }
+
+    public void OpenWoodTexture()
+    {
+        Application.OpenURL("https://assetstore.unity.com/publishers/9819");
+    }
+
+    public void OpenTileTexture()
+    {
+        Application.OpenURL("https://assetstore.unity.com/packages/2d/textures-materials/floors/20-man-made-ground-materials-12835");
+    }
+}
